c94fc64ae23010e9d923d95f7855a69149611fb3
regawan <andreaswager99@gmail.com>
2025-01-31 22:03:50 +0200

    Player setup.

--- player.gd
+++ player.gd
@@ -1,11 +1,43 @@
 extends Area2D
 
+@export var speed = 400 # How fast the player will move (pixels/sec).
+var screen_size # Size of the game window.
 
 # Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+func _ready():
+	screen_size = get_viewport_rect().size
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _process(delta: float):
+	# VARIABLES
+	var velocity = Vector2()
+
+	# ANIMATION CONTROL
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		$AnimatedSprite2D.play()
+	else:
+		$AnimatedSprite2D.stop()
+	if velocity.x != 0:
+		$AnimatedSprite2D.animation = "walk"
+		$AnimatedSprite2D.flip_v = false
+		# See the note below about the following boolean assignment.
+		$AnimatedSprite2D.flip_h = velocity.x < 0
+	elif velocity.y != 0:
+		$AnimatedSprite2D.animation = "up"
+		$AnimatedSprite2D.flip_v = velocity.y > 0
+
+	# INPUT
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 100
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= 100
+	if Input.is_action_pressed("move_down"):
+		velocity.y += 100
+	if Input.is_action_pressed("move_up"):
+		velocity.y -= 100
+
+	# MOVEMENT
+	position += velocity * delta
+	position = position.clamp(Vector2(), screen_size)
